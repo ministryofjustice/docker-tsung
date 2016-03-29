542d3532e82b05efa4903d23839cd1c23586f5ae
Niall Creech <niall.creech@digital.justice.gov.uk>
2016-03-29 17:51:50 +0100

    Add user root as crontab job owner

--- run-tsung.sh
+++ run-tsung.sh
@@ -16,10 +16,10 @@ fi
 cmd_tsung="tsung -l ${log_dir} -f ${tsung_config} start"
 
 if [ ! -z ${TSUNG_CRON_SCHEDULE} ];then
-    CRON_RUN_TSUNG="${TSUNG_CRON_SCHEDULE} ${cmd_tsung}"
+    CRON_RUN_TSUNG="${TSUNG_CRON_SCHEDULE} root ${cmd_tsung}"
 fi
 if [ ! -z ${TSUNG_S3_URL} ]; then
-    CRON_S3_BACKUP="*/10 * * * *  ${cmd_s3cmd}" 
+    CRON_S3_BACKUP="*/10 * * * *  root ${cmd_s3cmd}" 
 fi
 
 mkdir -p ${log_dir}
@@ -28,8 +28,8 @@ service ssh start
 
 if [ ! -z ${TSUNG_CRON_SCHEDULE} ]; then
     cat >> /etc/crontab <<_END
-        ${CRON_RUN_TSUNG}
-        ${CRON_S3_BACKUP}
+${CRON_RUN_TSUNG}
+${CRON_S3_BACKUP}
 _END
 else
     ${cmd_tsung} &
